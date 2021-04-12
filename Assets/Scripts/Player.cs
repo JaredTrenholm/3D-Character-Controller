aa8f14b9ea62b3a077ba33f348142be007081b8b
RoyalGuilds <Jared.trenholm12@gmail.com>
2021-04-12 09:12:12 -0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //ref: Code like Me https://www.youtube.com/channel/UCU9YE0hMnTt6TozuyVKicHA
+
+    private float forwardInput;
+    private float rightInput;
+
+    public CameraController cameraController;
+    public PlayerMovement playerMovement;
+
+    private Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (getVelocity() > 1)
+            {
+                playerMovement.SetMovementMode(MovementMode.Running);
+            }
+            else
+            {
+                playerMovement.SetMovementMode(MovementMode.Walking);
+            }
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            playerMovement.SetMovementMode(MovementMode.NarutoRun);
+        }
+        else
+        {
+            playerMovement.SetMovementMode(MovementMode.Walking);
+        }
+    }
+
+    public void AddMovementInput(float forward, float right)    // update movement inputs
+    {
+        forwardInput = forward;
+        rightInput = right;
+
+        Vector3 camFwd = cameraController.transform.forward;
+        Vector3 camRight = cameraController.transform.right;
+
+        Vector3 translation = forward * cameraController.transform.forward;
+        translation += right * cameraController.transform.right;
+        translation.y = 0;
+
+        if (translation.magnitude > 0)
+        {
+            velocity = translation;
+        }
+        else
+        {
+            velocity = Vector3.zero;
+        }
+        playerMovement.Velocity = translation;
+
+    }
+
+    public float getVelocity()
+    {
+        return playerMovement.Velocity.magnitude;
+    }
+
+    public void ToggleRun()
+    {
+        if (playerMovement.GetMovementMode() != MovementMode.Running)
+        {
+            playerMovement.SetMovementMode(MovementMode.Running);
+        }
+        else
+        {
+            playerMovement.SetMovementMode(MovementMode.Walking);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovementMode { Walking, Running, Sprinting, Crouching, Proning, NarutoRun }
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform t_mesh;
+    public float maxSpeed = 5.0f;
+    private float smoothSpeed;
+    private float rotationSpeed = 10.0f;
+    private PlayerAnimationController animatorScript;
+
+    public Rigidbody rigidbody;    
+
+    MovementMode movementMode;
+
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        animatorScript = this.gameObject.GetComponent<PlayerAnimationController>();
+        rigidbody = GetComponent<Rigidbody>();          //initialize rigibody reference
+    }
+
+    void Update()
+    {
+        // if player is moving, rotate player mesh to match camera facing.
+        if (velocity.magnitude > 0)
+        {
+            rigidbody.velocity = new Vector3(velocity.normalized.x * smoothSpeed, rigidbody.velocity.y, velocity.normalized.z * smoothSpeed);
+            smoothSpeed = Mathf.Lerp(smoothSpeed, maxSpeed, Time.deltaTime);
+            //t_mesh.rotation = Quaternion.LookRotation(velocity);
+            t_mesh.rotation = Quaternion.Lerp(t_mesh.rotation, Quaternion.LookRotation(velocity), Time.deltaTime * rotationSpeed);
+
+        }
+        else
+        {
+            smoothSpeed = Mathf.Lerp(maxSpeed, smoothSpeed, Time.deltaTime);
+        }
+    }
+
+    public Vector3 Velocity { get => rigidbody.velocity; set => velocity = value; }
+
+    public void SetMovementMode(MovementMode mode)
+    {
+        movementMode = mode;
+        switch (mode)
+        {
+            case MovementMode.Walking:
+                {
+                    maxSpeed = 5.0f;
+                    animatorScript.StopNarutoRun();
+                    break;
+                }   
+            case MovementMode.Running:
+                {
+                    maxSpeed = 10.0f;
+                    animatorScript.StopNarutoRun();
+                    break;
+                }
+            case MovementMode.Sprinting:
+                {
+                    maxSpeed = 20.0f;
+                    animatorScript.StopNarutoRun();
+                    break;
+                }
+            case MovementMode.Crouching:                
+                {
+                    maxSpeed = 4.0f;
+                    animatorScript.StopNarutoRun();
+                    break;
+                }
+            case MovementMode.Proning:
+                {
+                    maxSpeed = 2.0f;
+                    animatorScript.StopNarutoRun();
+                    break;
+                }
+            case MovementMode.NarutoRun:
+                {
+                    maxSpeed = 7.0f;
+                    break;
+                }
+        }
+    }
+
+    public MovementMode GetMovementMode()
+    {
+        return movementMode;
+    }
+}
